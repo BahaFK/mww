429349af8375fc12a224dfa85d9e38345390e1ed
BahaFK <bahaeddine.fekihsaid@eniso.u-sousse.tn>
2022-03-09 23:25:09 +0100

    wip:auth jwt

--- demo-api/src/SwaggerDecorator/SwaggerDecorator.php
+++ demo-api/src/SwaggerDecorator/SwaggerDecorator.php
@@ -54,7 +54,7 @@ final class SwaggerDecorator implements NormalizerInterface
 
         $tokenDocumentation = [
             'paths' => [
-                '/authentication_token' => [
+                '/api/authentication_token' => [
                     'post' => [
                         'tags' => ['Token'],
                         'operationId' => 'postCredentialsItem',
